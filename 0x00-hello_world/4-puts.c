20b385c7e860a1020a6e5ea464e73937cdcc6f2e
nickClouds1620 <ni.bediakopuni@gmail.com>
2023-07-13 18:06:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using puts function to display output
+ * Return: 0 (end)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
